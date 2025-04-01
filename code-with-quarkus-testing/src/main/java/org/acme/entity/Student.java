d670b32714651c9d434b12f5002c3b85d37212b5
mukulpandey2866 <mukulpandey2866@gmail.com>
2025-04-01 11:21:03 +0530

    msg one

--- code-with-quarkus-testing/src/main/java/org/acme/entity/Student.java
+++ code-with-quarkus-testing/src/main/java/org/acme/entity/Student.java
@@ -1,6 +1,8 @@
 package org.acme.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 import javax.annotation.processing.Generated;
 
@@ -8,6 +10,31 @@ import javax.annotation.processing.Generated;
 public class Student {
     @Id
     @GeneratedValue
+    Long studenStudentId;
+    String name;
+    String branch;
 
+    public Long getStudenStudentId() {
+        return studenStudentId;
+    }
 
+    public void setStudenStudentId(Long studenStudentId) {
+        this.studenStudentId = studenStudentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
 }
