7ee80787bf48b4d7ffd932582684b18a4c4aa852
mukulpandey2866 <mukulpandey2866@gmail.com>
2025-04-01 11:23:15 +0530

    msg one

--- code-with-quarkus-testing/src/main/java/org/acme/repository/StudentRepository.java
+++ code-with-quarkus-testing/src/main/java/org/acme/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package org.acme.repository;
 
-public class StudentRepository {
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.acme.entity.Student;
+
+@ApplicationScoped
+public class StudentRepository implements PanacheRepository<Student> {
+
 }
