fc5d5247a8d26bf51f44c721bf385436cb911cb8
mukulpandey2866 <mukulpandey2866@gmail.com>
2025-04-01 15:12:38 +0530

    msg one

--- code-with-quarkus-testing/src/main/java/org/acme/resource/StudentResource.java
+++ code-with-quarkus-testing/src/main/java/org/acme/resource/StudentResource.java
@@ -1,4 +1,27 @@
 package org.acme.resource;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.acme.entity.Student;
+import org.acme.repository.StudentRepository;
+
+import java.util.List;
+
+@Path("/")
 public class StudentResource {
+
+    @Inject
+    StudentRepository studentRepository;
+
+    @GET
+    @Path("/getStudentList")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getStudentList () {
+        List<Student> studentList = studentRepository.listAll();
+        return Response.ok(studentList).build();
+    }
 }
