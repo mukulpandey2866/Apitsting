b326b6ec3e09475055942b73afa5a38e02cee05a
mukulpandey2866 <mukulpandey2866@gmail.com>
2025-04-15 21:18:55 +0530

    msg one

--- code-with-quarkus-testing/src/test/java/org/acme/resource/StudentResourceTest.java
+++ code-with-quarkus-testing/src/test/java/org/acme/resource/StudentResourceTest.java
@@ -11,6 +11,7 @@ import jakarta.json.JsonObject;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import java.util.ArrayList;
 import java.util.List;
 import org.acme.entity.Student;
@@ -22,7 +23,14 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.CoreMatchers.*;
 
 @QuarkusTest
@@ -126,35 +134,98 @@ class StudentResourceTest {
 
   @Test
   void createStudent() {
+    Student s = new Student(1L,"rahul","CS");
+
+    Mockito.doNothing().when(studentRepository).persist(s);
+    Mockito.when(studentRepository.isPersistent(s)).thenReturn(true);
+
+    Response response = studentResource.createStudent(s);
+
+    assertNotNull(response);
+    assertNotNull(response.getEntity());
+    assertEquals(response.getStatus(), Status.CREATED.getStatusCode());
+    assertNotNull(response.getLocation());
+
   }
 
   @Test
   void getAllStudent() {
 
     List<Student> studentList = new ArrayList<>();
-    studentList.add(new Student(1L,"Shruti","CS"));
-    studentList.add(new Student(2L,"Rahul","CS"));
-    studentList.add(new Student(3L,"Akansha","CS"));
+    studentList.add(new Student(1L, "Shruti", "CS"));
+    studentList.add(new Student(2L, "Rahul", "CS"));
+    studentList.add(new Student(3L, "Akansha", "CS"));
 
     Mockito.when(studentRepository.listAll()).thenReturn(studentList);
 
+    Response response = studentResource.getAllStudent();
+
+    assertNotNull(response);
+    assertNotNull(response.getEntity());
+    assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+
+    List<Student> studentList1 = (List<Student>) response.getEntity();
+    assertEquals(studentList.size(), 3);
+
   }
+
+
+
+    @Test
+    void testGetStudentById () {
+
+      Student student1 = new Student(4L, "Rajesh", "ME");
+      Mockito.when(studentRepository.findById(ArgumentMatchers.any(Long.class))).thenReturn(student1);
+
+      Response response = studentResource.getStudentById(4L);
+      assertNotNull(response);
+      assertNotNull(response.getEntity());
+      assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+
+      Student student2 = (Student) response.getEntity();
+
+      assertEquals(student2.getName(), "Rajesh");
+      assertEquals(student2.getBranch(), "ME");
+
+
+    }
 
   @Test
-  void getCSStudentList() {
+  void testGetStudentByIdKO () {
+
+    Student student1 = new Student(4L, "Rajesh", "ME");
+    Mockito.when(studentRepository.findById(ArgumentMatchers.any(Long.class))).thenReturn(null);
+
+    Response response = studentResource.getStudentById(4L);
+    assertNotNull(response);
+    assertEquals(response.getStatus(), Status.NO_CONTENT.getStatusCode());
+
   }
 
-  @Test
-  void testGetStudentById() {
+    @Test
+    void updateStudent () {
+
+      Student student1 = new Student(4L, "Rajesh", "ME");
+      Mockito.when(studentRepository.findById(ArgumentMatchers.any(Long.class))).thenReturn(student1);
+
+      Student newUpdateStudent = new Student();
+      newUpdateStudent.setName("Mahesh");
+      Response response=studentResource.updateStudent(newUpdateStudent,4L);
+
+      assertNotNull(response);
+      assertNotNull(response.getEntity());
+      assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+
+      Student sr=(Student) response.getEntity();
+
+      assertEquals(sr.getName(), "Mahesh");
+      assertEquals(sr.getBranch(), "ME");
+
+    }
+
+
   }
 
-  @Test
-  void updateStudent() {
-  }
-
-  @Test
-  void testUpdateStudent() {
-  }
-
-} //add quarkus h2 depedency
+//}
+//add quarkus h2 depedency
 // write test cases for PUT(check with updated id) and DELETE(size-1).
